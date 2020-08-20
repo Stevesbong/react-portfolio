c7590514acd651154c44f028ca473719dab7ab04
Steve Park <ghkfkd0323@gmail.com>
2020-08-20 01:54:30 -0700

    add data.json file for each project name desc etc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import ProjectContainer from './components/ProjectContainer';
+import Project from './components/Project';
+import Header from './components/Header';
+
 
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+
+      <div className="App">
+
+        <Header />
+        <ProjectContainer />
+        <Switch>
+          <Route></Route>
+        </Switch>
+
+      </div>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Data from '../Data.json';
 
 export default class ProjectContainer extends Component {
+
+    state = {
+        data: Data
+    }
     render() {
+        const { data } = this.state;
+        console.log(data.projects)
         return (
             <div>
-                
+                {data.projects.map( ( project,index ) => (
+                    <div key={index}>
+                        <p>{project.project_name}</p>
+                        <p>{project.description}</p>
+                        <p>{project.github_link}</p>
+                        <p>{project.live_link}</p>
+                        <ul>
+                            {project.image_urls.map( ( img, index ) => (
+                                <li key={index}>{img}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
             </div>
         )
     }
